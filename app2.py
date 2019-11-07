96b8155f9f06410313db573feec1e39e3bca885b
guidomlima <guilhermedlima@bol.com.br>
2019-11-07 17:32:33 -0300

    new file:   __pycache__/constants.cpython-37.pyc 	modified:   app.py 	new file:   app2.py 	new file:   constants.py 	modified:   db.sqlite 	modified:   media/temp.jpg 	modified:   templates/cadastro.html 	modified:   templates/lista.html

--- app2.py
+++ app2.py
@@ -1,9 +1,12 @@
 #coding:utf-8
-from flask import Flask, render_template, request, url_for, redirect, jsonify, send_from_directory, send_file
+from flask import Flask, render_template, request, url_for,\
+    redirect, jsonify, make_response, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from werkzeug.middleware.shared_data import SharedDataMiddleware
-import datetime, os
+from werkzeug.security import generate_password_hash,\
+    check_password_hash
+from functools import wraps
+import datetime, uuid, jwt, time
+import constants as USER
 import urllib.request
 
 #app = Flask(__name__)
@@ -12,10 +15,12 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'campainha-secret'
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-app.config['MEDIA_ROOT'] = os.path.join(PROJECT_ROOT, 'media')
+app.config['CSRF_ENABLED'] = True
+app.config['CSRF_SESSION_KEY'] = 'SOMETHING_IMPOSSIBLE_TO_GUEES'
+
+db = SQLAlchemy(app)
 
 def converter_datetime(value):
     #Converte datetime object em string para processamento JSON
@@ -23,6 +28,141 @@ def converter_datetime(value):
         return None
     return [value.strftime("%Y-%m-%d"), value.strftime("%H:%M:%S")]
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    public_id = db.Column(db.String(50), unique=True)
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
+    role = db.Column(db.SmallInteger, default=USER.USER)
+    status = db.Column(db.SmallInteger, default=USER.NEW)
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args,**kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return jsonify({'message': 'Token is missing'}),401
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'message': 'Token is invalid'}), 401
+
+        return f(current_user, *args, **kwargs)
+    return decorated
+
+@app.route('/user', methods=['GET'])
+@token_required
+def get_all_users(current_user):
+
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot perform that function'}), 401
+
+    users = User.query.all()
+    output = []
+    for user in users:
+        user_data = {}
+        user_data['public_id'] = user.public_id
+        user_data['username'] = user.username
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+        output.append(user_data)
+    return jsonify(output)
+
+@app.route('/user/<public_id>', methods=['GET'])
+@token_required
+def get_one_user(current_user, public_id):
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot perform that function'}), 401
+
+    user = User.query.filter_by(public_id=public_id).first()
+    if not user:
+        return jsonify({'message': 'User not found'})
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['username'] = user.username
+    user_data['password'] = user.password
+    user_data['admin'] = user.admin
+    return jsonify(user_data)
+
+@app.route('/user', methods=['POST'])
+@token_required
+def create_user(current_user):
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot perform that function'}), 401
+
+    data = request.get_json()
+    hashed_password = generate_password_hash(data['password'],method='sha256')
+    new_user = User(public_id=str(uuid.uuid4()), username=data['username'], password=hashed_password, admin=False)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'New user created'})
+
+@app.route('/user/<public_id>', methods=['PUT'])
+@token_required
+def upgrade_user(current_user, public_id):
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot perform that function'}), 401
+
+    data = request.get_json()
+    user = User.query.filter_by(public_id=public_id).first()
+    if not user:
+        return jsonify({'message': 'User not found'})
+    user.admin = data['admin']
+    user.password = generate_password_hash(data['password'],method='sha256')
+    db.session.commit()
+    return jsonify({'message': 'The user has been updated'})
+
+@app.route('/user/<public_id>', methods=['DELETE'])
+@token_required
+def delete_user(current_user, public_id):
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot perform that function'}), 401
+
+    user = User.query.filter_by(public_id=public_id).first()
+    if not user:
+        return jsonify({'message': 'User not found'})
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message': 'The user has been deleted'})
+
+@app.route('/login', methods=['POST'])
+def login():
+    auth = request.authorization
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify',401,{'WWW-Authenticate': 'Basic realm="Login required"'})
+    user = User.query.filter_by(username=auth.username).first()
+    if not user:
+        return make_response('Could not verify',401,{'WWW-Authenticate': 'Basic realm="Login required"'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'public_id': user.public_id,
+                            'exp': datetime.datetime.utcnow()+
+                                   datetime.timedelta(minutes=30)
+                            }, app.config['SECRET_KEY'])
+        return jsonify({'token': token.decode('UTF-8')})
+
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required"'})
+
+@app.route('/login/<token>', methods=['GET'])
+def current_user(token):
+    if not token:
+        return jsonify({'message': 'Token not found'}), 401
+    try:
+        data = jwt.decode(token, app.config['SECRET_KEY'])
+        current_user = User.query.filter_by(public_id=data['public_id']).first()
+    except:
+        return jsonify({'message': 'Token is invalid'}), 401
+    user_data = {}
+    user_data['username'] = current_user.username
+    user_data['admin'] = current_user.admin
+    return jsonify(user_data)
+
+
+'''
 class Usuario(db.Model):
     __tablename__='usuario'
     _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -40,7 +180,7 @@ class Usuario(db.Model):
             'nome': self.nome,
             'senha_hash': self.senha_hash
         }
-
+'''
 class Camera(db.Model):
     __tablename__ = 'camera'
     _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -65,7 +205,7 @@ class Notificacao(db.Model):
     dataehora = db.Column(db.DateTime, default=datetime.datetime.now)
     status = db.Column(db.String)
 
-    def __init__(self, dataehora, status):
+    def __init__(self, status):
         self.status = status
 
     #usar marshmallow para serialização complexas
@@ -89,9 +229,9 @@ class Gpio(db.Model):
     pin = db.Column(db.Integer, unique=True)
     status = db.Column(db.Integer)
 
-    def __init__(self, pin, status):
+    def __init__(self, pin):
         self.pin = pin
-        self.status = status
+        self.status = 0
 
     @property
     def serialize(self):
@@ -102,7 +242,14 @@ class Gpio(db.Model):
         }
 
 db.create_all()
+user = User.query.filter_by(username='admin').first()
+if not user:
+    hashed_password = generate_password_hash('admin', method='sha256')
+    new_user = User(public_id=str(uuid.uuid4()), username='admin', password=hashed_password, admin=True)
+    db.session.add(new_user)
+    db.session.commit()
 
+'''
 @app.route('/', methods=['GET'])
 def home():
     lista_usuario = Usuario.query.all()
@@ -123,7 +270,8 @@ def senha(senha):
         return jsonify(json_list), 404
     json_list=[{'message': 'Usuário aceito'}]    
     return jsonify(json_list), 200
-
+'''
+@token_required
 @app.route('/camera', methods=['GET'])
 def listar_cameras():
     lista_cameras = Camera.query.all()
@@ -151,11 +299,11 @@ def procura_notificacao(status):
     if not notificacao:
         json_list=[{'message': 'Não há notificacao com esse status'}]
         return jsonify(json_list), 206
-    json_list=[{
+    json_list={
             '_id': notificacao._id,
             'dataehora': notificacao.dataehora,
             'status': notificacao.status,
-        }]
+        }
     return jsonify(json_list), 200
 
 @app.route('/gpio', methods=['GET'])
@@ -195,8 +343,8 @@ def cadastrar_notificacao():
     db.session.commit()
     return data, 201
 
-@app.route("/camera", methods=['PUT'])
-def atualizar_camera():
+@app.route("/camera/", methods=['PUT'])
+def atualizar_camera(descricao):
     data = request.get_json()
     jCamera = Camera(**data)
     camera = Camera.query.filter_by(descricao=jCamera.descricao).first()
@@ -208,11 +356,11 @@ def atualizar_camera():
     json_list=[{'message': 'Atualizado com sucesso'}]
     return jsonify(json_list), 200
 
-@app.route("/notificacao", methods=['PUT'])
-def atualizar_notificacao():
+@app.route("/notificacao/<string:status>", methods=['PUT'])
+def atualizar_notificacao(status):
     data = request.get_json()
     jNotificacao = Notificacao(**data)
-    notificacao = Notificacao.query.first()
+    notificacao = Notificacao.query.filter_by(status=status).order_by(Notificacao._id.desc()).first()
     if not notificacao:
         json_list=[{'message': 'Não há notificação criada'}]
         return jsonify(json_list), 404
@@ -220,20 +368,19 @@ def atualizar_notificacao():
     notificacao.dataehora=datetime.datetime.now()
     db.session.commit()
     json_list=[{'message': 'Atualizado com sucesso'}]
-    return jsonify(json_list), 200
+    return jsonify(notificacao), 200
 
 @app.route("/gpio", methods=['PUT'])
-def atualizar_gpio():
+def atualizar_gpio(pin):
     data = request.get_json()
     jGpio = Gpio(**data)
     gpio = Gpio.query.filter_by(pin=jGpio.pin).first()
     if not gpio:
-        json_list=[{'message': 'Não há gpio com esse pin'}]
+        json_list=[{'message': 'Não há gpio com esse id'}]
         return jsonify(json_list), 404
     gpio.status=jGpio.status
     db.session.commit()
-    json_list=[{'message': 'Atualizado com sucesso'}]
-    return jsonify(json_list), 200
+    return jsonify(gpio), 200
 
 @app.route("/camera/<int:id>", methods=['DELETE'])
 def remover_camera(id):
@@ -257,7 +404,7 @@ def cadastrar():
 
 @app.route("/lista")
 def lista():
-    usuarios = Usuario.query.all()
+    usuarios = User.query.all()
     cameras = Camera.query.all()
     notificacoes = Notificacao.query.all()
     gpios = Gpio.query.all()
@@ -269,15 +416,17 @@ def lista():
 @app.route("/cadastroUsuario",methods=['GET', 'POST'])
 def cadastroUsuario():
     if request.method == "POST":
-        nome = request.form.get("nome")
-        senha = request.form.get("senha")
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-        if nome and senha:
-            u = Usuario(nome, senha)
-            db.session.add(u)
+        if username and password:
+            hashed_password = generate_password_hash(data['password'],method='sha256')
+            new_user = User(public_id=str(uuid.uuid4()), username=data['username'], password=hashed_password, admin=False)
+            db.session.add(new_user)
             db.session.commit()
     return redirect(url_for("index"))
 
+'''
 @app.route("/excluirUsuario/<int:id>")
 def excluirUsuario(id):
     usuario = Usuario.query.filter_by(_id=id).first()
@@ -306,7 +455,7 @@ def atualizarUsuario(id):
             return redirect(url_for("lista"))
 
     return render_template("atualizarUsuario.html", usuario=usuario)
-
+'''
 @app.route("/cadastroCamera",methods=['GET', 'POST'])
 def cadastroCamera():
     if request.method == "POST":
@@ -368,6 +517,7 @@ def atualizarNotificacao(id):
     notificacao = Notificacao.query.filter_by(_id=id).first()
     if request.method == "POST":
         status = request.form.get("status")
+
         if status:
             notificacao.dataehora=datetime.datetime.now()
             notificacao.status=status
@@ -380,7 +530,7 @@ def cadastroGpio():
     if request.method == "POST":
         pin = request.form.get("pin")
 
-        if pin and status:
+        if pin:
             g = Gpio(pin)
             db.session.add(g)
             db.session.commit()
@@ -399,20 +549,20 @@ def atualizarGpio(id):
     gpio = Gpio.query.filter_by(_id=id).first()
     if request.method == "POST":
         status = request.form.get("status")
+
         if status:
             gpio.status = status
             db.session.commit()
             return redirect(url_for("lista"))
 
     return render_template("atualizarGpio.html", gpio=gpio)
-    
 @app.route("/imagem")
 def baixarImagem():
-    endereco='http://192.168.15.2:8080/shot.jpg'
+    endereco='http://192.168.0.107:8080/shot.jpg'
     with urllib.request.urlopen(endereco) as url:
         with open('media/temp.jpg','wb') as f:
             f.write(url.read())
-    return send_from_directory(app.config.get('MEDIA_ROOT'), 'temp.jpg')
+    return send_from_directory('media/', 'temp.jpg')
 
 #remover debug e use_reloader para deploy
 if __name__ == "__main__":
